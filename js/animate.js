045385d329f553f063c343245452fff50e9238ef
Denis Chernyavskiy <79461641+osmosx@users.noreply.github.com>
2021-05-27 11:26:12 +0300

    Сжаты картинки, добавлены мелкие изминения

--- js/animate.js
+++ js/animate.js
@@ -2,3 +2,5 @@ const element_two = document.querySelector('h1');
 const element = document.querySelector('img');
 element.style.setProperty('--animate-duration', '5.0s');
 element_two.style.setProperty('--animate-duration', '3.0s');
+
+
